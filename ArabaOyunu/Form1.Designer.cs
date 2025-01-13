41568d12845f7ea5f88632bfbde6a670c6e50f9d
sametyesilot <144464600+sametyesilot@users.noreply.github.com>
2025-01-13 14:23:19 +0300

    Araba Oyunu
    c# araba oyunu

---
+++ ArabaOyunu/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace ArabaOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelSolSerit6 = new System.Windows.Forms.Label();
+            this.labelSagfSerit6 = new System.Windows.Forms.Label();
+            this.labelSagfSerit5 = new System.Windows.Forms.Label();
+            this.labelSolSerit5 = new System.Windows.Forms.Label();
+            this.labelSagfSerit3 = new System.Windows.Forms.Label();
+            this.labelSolSerit3 = new System.Windows.Forms.Label();
+            this.labelSagfSerit4 = new System.Windows.Forms.Label();
+            this.labelSolSerit4 = new System.Windows.Forms.Label();
+            this.labelSagfSerit1 = new System.Windows.Forms.Label();
+            this.labelSolSerit1 = new System.Windows.Forms.Label();
+            this.labelSagfSerit2 = new System.Windows.Forms.Label();
+            this.labelSolSerit2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.redCar = new System.Windows.Forms.PictureBox();
+            this.timerSerit = new System.Windows.Forms.Timer(this.components);
+            this.timerRandomCar = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.redCar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(-4, -8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 730);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(691, -8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 730);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // labelSolSerit6
+            // 
+            this.labelSolSerit6.BackColor = System.Drawing.Color.White;
+            this.labelSolSerit6.Location = new System.Drawing.Point(223, -94);
+            this.labelSolSerit6.Name = "labelSolSerit6";
+            this.labelSolSerit6.Size = new System.Drawing.Size(11, 110);
+            this.labelSolSerit6.TabIndex = 3;
+            // 
+            // labelSagfSerit6
+            // 
+            this.labelSagfSerit6.BackColor = System.Drawing.Color.White;
+            this.labelSagfSerit6.Location = new System.Drawing.Point(464, -94);
+            this.labelSagfSerit6.Name = "labelSagfSerit6";
+            this.labelSagfSerit6.Size = new System.Drawing.Size(11, 110);
+            this.labelSagfSerit6.TabIndex = 4;
+            // 
+            // labelSagfSerit5
+            // 
+            this.labelSagfSerit5.BackColor = System.Drawing.Color.White;
+            this.labelSagfSerit5.Location = new System.Drawing.Point(464, 31);
+            this.labelSagfSerit5.Name = "labelSagfSerit5";
+            this.labelSagfSerit5.Size = new System.Drawing.Size(11, 110);
+            this.labelSagfSerit5.TabIndex = 6;
+            // 
+            // labelSolSerit5
+            // 
+            this.labelSolSerit5.BackColor = System.Drawing.Color.White;
+            this.labelSolSerit5.Location = new System.Drawing.Point(223, 31);
+            this.labelSolSerit5.Name = "labelSolSerit5";
+            this.labelSolSerit5.Size = new System.Drawing.Size(11, 110);
+            this.labelSolSerit5.TabIndex = 5;
+            // 
+            // labelSagfSerit3
+            // 
+            this.labelSagfSerit3.BackColor = System.Drawing.Color.White;
+            this.labelSagfSerit3.Location = new System.Drawing.Point(464, 285);
+            this.labelSagfSerit3.Name = "labelSagfSerit3";
+            this.labelSagfSerit3.Size = new System.Drawing.Size(11, 110);
+            this.labelSagfSerit3.TabIndex = 10;
+            // 
+            // labelSolSerit3
+            // 
+            this.labelSolSerit3.BackColor = System.Drawing.Color.White;
+            this.labelSolSerit3.Location = new System.Drawing.Point(223, 285);
+            this.labelSolSerit3.Name = "labelSolSerit3";
+            this.labelSolSerit3.Size = new System.Drawing.Size(11, 110);
+            this.labelSolSerit3.TabIndex = 9;
+            // 
+            // labelSagfSerit4
+            // 
+            this.labelSagfSerit4.BackColor = System.Drawing.Color.White;
+            this.labelSagfSerit4.Location = new System.Drawing.Point(464, 160);
+            this.labelSagfSerit4.Name = "labelSagfSerit4";
+            this.labelSagfSerit4.Size = new System.Drawing.Size(11, 110);
+            this.labelSagfSerit4.TabIndex = 8;
+            // 
+            // labelSolSerit4
+            // 
+            this.labelSolSerit4.BackColor = System.Drawing.Color.White;
+            this.labelSolSerit4.Location = new System.Drawing.Point(223, 160);
+            this.labelSolSerit4.Name = "labelSolSerit4";
+            this.labelSolSerit4.Size = new System.Drawing.Size(11, 110);
+            this.labelSolSerit4.TabIndex = 7;
+            // 
+            // labelSagfSerit1
+            // 
+            this.labelSagfSerit1.BackColor = System.Drawing.Color.White;
+            this.labelSagfSerit1.Location = new System.Drawing.Point(464, 540);
+            this.labelSagfSerit1.Name = "labelSagfSerit1";
+            this.labelSagfSerit1.Size = new System.Drawing.Size(11, 110);
+            this.labelSagfSerit1.TabIndex = 14;
+            // 
+            // labelSolSerit1
+            // 
+            this.labelSolSerit1.BackColor = System.Drawing.Color.White;
+            this.labelSolSerit1.Location = new System.Drawing.Point(223, 540);
+            this.labelSolSerit1.Name = "labelSolSerit1";
+            this.labelSolSerit1.Size = new System.Drawing.Size(11, 110);
+            this.labelSolSerit1.TabIndex = 13;
+            // 
+            // labelSagfSerit2
+            // 
+            this.labelSagfSerit2.BackColor = System.Drawing.Color.White;
+            this.labelSagfSerit2.Location = new System.Drawing.Point(464, 415);
+            this.labelSagfSerit2.Name = "labelSagfSerit2";
+            this.labelSagfSerit2.Size = new System.Drawing.Size(11, 110);
+            this.labelSagfSerit2.TabIndex = 12;
+            // 
+            // labelSolSerit2
+            // 
+            this.labelSolSerit2.BackColor = System.Drawing.Color.White;
+            this.labelSolSerit2.Location = new System.Drawing.Point(223, 415);
+            this.labelSolSerit2.Name = "labelSolSerit2";
+            this.labelSolSerit2.Size = new System.Drawing.Size(11, 110);
+            this.labelSolSerit2.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(0, 650);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(710, 49);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "label3";
+            // 
+            // redCar
+            // 
+            this.redCar.Image = global::ArabaOyunu.Properties.Resources.car3;
+            this.redCar.Location = new System.Drawing.Point(296, 428);
+            this.redCar.Name = "redCar";
+            this.redCar.Size = new System.Drawing.Size(120, 200);
+            this.redCar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.redCar.TabIndex = 16;
+            this.redCar.TabStop = false;
+            // 
+            // timerSerit
+            // 
+            this.timerSerit.Enabled = true;
+            this.timerSerit.Interval = 200;
+            this.timerSerit.Tick += new System.EventHandler(this.timerSerit_Tick);
+            // 
+            // timerRandomCar
+            // 
+            this.timerRandomCar.Enabled = true;
+            this.timerRandomCar.Interval = 200;
+            this.timerRandomCar.Tick += new System.EventHandler(this.timerRandomCar_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(705, 698);
+            this.Controls.Add(this.redCar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelSagfSerit1);
+            this.Controls.Add(this.labelSolSerit1);
+            this.Controls.Add(this.labelSagfSerit2);
+            this.Controls.Add(this.labelSolSerit2);
+            this.Controls.Add(this.labelSagfSerit3);
+            this.Controls.Add(this.labelSolSerit3);
+            this.Controls.Add(this.labelSagfSerit4);
+            this.Controls.Add(this.labelSolSerit4);
+            this.Controls.Add(this.labelSagfSerit5);
+            this.Controls.Add(this.labelSolSerit5);
+            this.Controls.Add(this.labelSagfSerit6);
+            this.Controls.Add(this.labelSolSerit6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Araba Yarışı";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.redCar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelSolSerit6;
+        private System.Windows.Forms.Label labelSagfSerit6;
+        private System.Windows.Forms.Label labelSagfSerit5;
+        private System.Windows.Forms.Label labelSolSerit5;
+        private System.Windows.Forms.Label labelSagfSerit3;
+        private System.Windows.Forms.Label labelSolSerit3;
+        private System.Windows.Forms.Label labelSagfSerit4;
+        private System.Windows.Forms.Label labelSolSerit4;
+        private System.Windows.Forms.Label labelSagfSerit1;
+        private System.Windows.Forms.Label labelSolSerit1;
+        private System.Windows.Forms.Label labelSagfSerit2;
+        private System.Windows.Forms.Label labelSolSerit2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox redCar;
+        private System.Windows.Forms.Timer timerSerit;
+        private System.Windows.Forms.Timer timerRandomCar;
+    }
+}
+
---
+++ ArabaOyunu/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArabaOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int seritSayisi = 1;
+        Random R = new Random();
+
+        class RandomCar
+        {
+            public bool FakeHaveCar = false;
+            public PictureBox FakeCar;
+            public bool vakit = false;
+
+        }
+
+        RandomCar[] rndCar = new RandomCar[2];
+        void BringRandomCar(PictureBox pb)
+        {
+            int rnd = R.Next(0,4);
+
+            switch (rnd)
+            {
+                case 0:
+                    pb.Image = Properties.Resources.car0;
+                    break;
+                case 1:
+                    pb.Image = Properties.Resources.car1;
+                    break;
+                case 2:
+                    pb.Image = Properties.Resources.car2;
+                    break;
+                case 3:
+                    pb.Image = Properties.Resources.car3;
+                    break;
+            }
+            pb.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+
+
+        private void AracYerine()
+        {
+            if (seritSayisi==1)
+            {
+                redCar.Location = new Point(223, 328);
+            }
+            else if (seritSayisi == 0)
+            {
+                redCar.Location = new Point(50, 328);
+            }
+            else if (seritSayisi == 2)
+            {
+                redCar.Location = new Point(400, 328);
+            }
+        }
+        private void label19_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelSpeed_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (var i = 0; i < rndCar.Length; i++)
+            {
+                rndCar[i] = new RandomCar();
+            }
+            rndCar[0].vakit = true;
+            AracYerine();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Right)
+            {
+                if (seritSayisi < 2)
+                {
+                    seritSayisi++;
+                }
+            }else if(e.KeyCode == Keys.Left)
+            {
+                if (seritSayisi > 0)
+                    seritSayisi--;
+            }
+            AracYerine();
+        }
+
+        private void timerRandomCar_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < rndCar.Length; i++)
+            {
+                if (!rndCar[i].FakeHaveCar && rndCar[i].vakit)
+                {
+                    rndCar[i].FakeCar = new PictureBox();
+                    BringRandomCar(rndCar[i].FakeCar);
+                    rndCar[i].FakeCar.Size = new Size(120, 200);
+                    rndCar[i].FakeCar.Top = -rndCar[i].FakeCar.Height;
+
+                    int seriteYerlestir = R.Next(0,3);
+
+                    if (seriteYerlestir == 0)
+                    {
+                        rndCar[i].FakeCar.Left = 50;
+                    }
+                    else if (seriteYerlestir == 1)
+                    {
+                        rndCar[i].FakeCar.Left = 223;
+                    }
+                    else if (seriteYerlestir == 2)
+                    {
+                        rndCar[i].FakeCar.Left = 400;
+                    }
+                    this.Controls.Add(rndCar[i].FakeCar);
+                    rndCar[i].FakeHaveCar = true;
+                }
+                else
+                {
+                    if (rndCar[i].vakit)
+                    {
+                        rndCar[i].FakeCar.Top += 20;
+                        if (rndCar[i].FakeCar.Top >=154)
+                        {
+                            for (int j = 0; j < rndCar.Length; j++)
+                            {
+                                if (!rndCar[j].vakit)
+                                {
+                                    rndCar[j].vakit = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                    if (rndCar[i].FakeCar != null && rndCar[i].FakeCar.Top >= this.Height - 20)
+                    {
+                        rndCar[i].FakeCar.Dispose();
+                        rndCar[i].FakeCar = null; // Dispose sonrası referansı temizle
+                        rndCar[i].FakeHaveCar = false;
+                        rndCar[i].vakit = false;
+                    }
+                }
+
+
+                if (rndCar[i].vakit)
+                {
+                    if (rndCar[i].FakeCar == null)
+                        continue; // FakeCar yoksa işlemleri atla
+
+                    float mutlakX = Math.Abs(((redCar.Left + (redCar.Width / 2)) - (rndCar[i].FakeCar.Left + (rndCar[i].FakeCar.Width / 2))));
+                    float mutlakY = Math.Abs(((redCar.Top + (redCar.Height / 2)) - (rndCar[i].FakeCar.Top + (rndCar[i].FakeCar.Height / 2))));
+                    float farkGenislik = (redCar.Width / 2) + (rndCar[i].FakeCar.Width / 2);
+                    float farkYukseklik = (redCar.Height / 2) + (rndCar[i].FakeCar.Height / 2);
+
+                    if (farkGenislik > mutlakX && farkYukseklik > mutlakY)
+                    {
+                        timerRandomCar.Enabled = false;
+                        timerSerit.Enabled = false;
+                        DialogResult dr = MessageBox.Show("Kaza Yaptın. Tekrar Denemek İster Misin.", "Game Over", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                        if (dr == DialogResult.Yes)
+                        {
+                            AracYerine();
+                            for (int j = 0; j < rndCar.Length; j++)
+                            {
+                                if (rndCar[j].FakeCar != null)
+                                {
+                                    rndCar[j].FakeCar.Dispose(); // Dispose işlemi
+                                    rndCar[j].FakeCar = null;    // Referansı temizle
+                                }
+                                rndCar[j].FakeHaveCar = false;
+                                rndCar[j].vakit = false;
+                            }
+
+                            rndCar[0].vakit = true;
+                            timerRandomCar.Enabled = true;
+                            timerRandomCar.Interval = 200;
+
+                            timerSerit.Enabled = true;
+                            timerSerit.Interval = 200;
+                        }
+                    }
+                }
+
+
+            }
+        }
+
+        bool seritHareket = false;
+        private void timerSerit_Tick(object sender, EventArgs e)
+        {
+            if (seritHareket==false)
+            {
+                for (int i = 1; i < 7; i++)
+                {
+                    this.Controls.Find("labelSolSerit" + i.ToString(), true)[0].Top -= 25;
+                    this.Controls.Find("labelSagfSerit" + i.ToString(), true)[0].Top -= 25;
+                    seritHareket = true;
+                }
+            }
+            else
+            {
+                for (int i = 1; i < 7; i++)
+                {
+                    this.Controls.Find("labelSolSerit" + i.ToString(), true)[0].Top += 25;
+                    this.Controls.Find("labelSagfSerit" + i.ToString(), true)[0].Top += 25;
+                    seritHareket = false;
+                }
+            }
+        }
+    }
+}
